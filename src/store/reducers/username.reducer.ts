7221b4ed44080ee4643e65b3bef05c241f1bb42e
saiumesh535 <d.sai535@gmail.com>
2018-11-05 12:18:34 +0530

    now supports multiple action listener 💖💖

--- src/store/reducers/username.reducer.ts
+++ src/store/reducers/username.reducer.ts
@@ -1,6 +1,6 @@
 import { Reducer, Action, IState, IReducer } from 'angular-store';
 import { IUsername, AppState } from '../types';
-import { UPDATE_USERNAME } from '../actions';
+import { UPDATE_USERNAME, ADD_USER } from '../actions';
 
 
 const username: IReducer<string> = {
@@ -17,6 +17,12 @@ export class UsernameReducer {
   public updateUsername(payload: string, { getState, updateState }: IState) {
     const state = getState<AppState>();
     updateState({ key: username.key, payload });
+  }
+
+  // multi action
+  @Action([UPDATE_USERNAME, ADD_USER ])
+  public onMultiUpdate(payload: string, state: IState) {
+    console.log('both changes!!', payload);
   }
 
 }
